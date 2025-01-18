120535dfd767a4032a0d1c988a1db68939b2e897
Nikhil Bansal <114744576+Nik26sal@users.noreply.github.com>
2025-01-18 14:01:56 +0530

    main

---
+++ backend/models/bookModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+    bookName:{
+        type:String,
+        required:true
+    },
+    bookAuthorName:{
+        type:String,
+        required:true
+    },
+    publishedYear:{
+        type:Number,
+        required:true
+    },
+    User:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Admin'
+    },
+    bookImage:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+})
+
+export const Book = mongoose.model('Book',bookSchema);
---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        minLength:8,
+        required:true
+    },
+    refreshToken:{
+        type:String
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    savedBook:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Book'
+    }]
+},{
+    timestamps:true
+})
+
+export const User = mongoose.model('User',userSchema)
