e8d02e7206e35e543faf7df06e4faf7cd54e2514
ankitsingh <147189008+AnkitSinghD10S@users.noreply.github.com>
2025-01-18 23:10:51 +0530

    add authentication and book routes; refactor server setup

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,29 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import authRouter from './routes/authRoute.js';
+import bookRouter from './routes/bookRoute.js';
+
 dotenv.config();
 const app= express();
 const MONGOOSE = process.env.MONGOOSE;
 const PORT = process.env.PORT||5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-mongoose.connect(MONGOOSE).then(() => {
-    console.log('Database connected');
-    app.listen(PORT, () => {
-        console.log('Server has started');
+
+app.use(express.json());
+
+app.use('/api/auth', authRouter
+
+app.use('/api/book',bookRouter)
+
+try {
+    mongoose.connect(MONGOOSE).then(() => {
+        console.log('Database connected');
+        app.listen(PORT, () => {
+            console.log('Server has started');
+        });
     });
-});
+} catch (error) {
+    console.log('error has occured while connecting the database',error);
+}
 
