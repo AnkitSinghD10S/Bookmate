559629e611345cfc0729bbaeade31e8adf6c5264
ankitsingh <147189008+AnkitSinghD10S@users.noreply.github.com>
2025-02-02 21:29:24 +0530

    Add CORS support, improve error handling, and implement custom hook for fetching books

--- frontend/src/Component/Nav/nav.jsx
+++ frontend/src/Component/Nav/nav.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import  { useEffect } from 'react';
 import './nav.css';
-import { IoSearch } from "react-icons/io5";
+// import { IoSearch } from "react-icons/io5";
 function Nav() {
   useEffect(() => {  
     const handleScroll = () => {
@@ -27,12 +27,12 @@ function Nav() {
       <div className='nav-mid'>
           <input type="text" id="search-bar" placeholder="Search your books..." />
           <div className='search-icon'>
-             <IoSearch />
+             {/* <IoSearch /> */}
           </div>
       </div>
 
       
-      <div class='nav-right'>
+      <div className='nav-right'>
         <li><a href="#">Home</a></li>
         <li><a href="#">Contact</a></li>
         <li><a href="/About">About us</a></li>
