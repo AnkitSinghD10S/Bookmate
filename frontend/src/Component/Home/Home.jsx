d1fc5edb4dddb02b1559df2d70a73947c5bce5f9
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-02-09 22:55:41 +0530

    Code to be commited for frontend button isseue

--- frontend/src/Component/Home/Home.jsx
+++ frontend/src/Component/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home(){
         </div>
         <h1 id='heading-name'>BooꓘMate</h1>
         <h2 id='abouttitle'>Your Ultimate Companion for Seamless Book Management</h2>
-        <div className='btn'>
+        <div className='bt'>
           <button type="button" id='btn1'>Subscribe</button>
           <button type='button' id='btn2'>7-Days Trial</button>
         </div>
