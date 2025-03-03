01236e0424546812538ad7ff224bb14f3e82de1d
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-03-03 23:04:54 +0530

    Error Fixed: Page Rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Nav from "./Component/Nav/Nav";
+import Nav from "./Component/Nav/nav";
 import Footer from "./Component/Footer/footer";
 import { Outlet } from "react-router";
 
